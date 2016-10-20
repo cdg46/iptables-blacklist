1065a78b68c90c17908079b5f91911274b12d441
Alexander Kress <alexanderkress@users.noreply.github.com>
2016-10-20 22:54:15 +0200

    Update iptables-blacklist.sh

--- iptables-blacklist.sh
+++ iptables-blacklist.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+echo ===================
+echo  iptables-backlist
+echo ===================
+echo
+echo The following IP addresses will be blocked:
+
 for ip in $(wget -O- -q https://raw.githubusercontent.com/alexanderkress/iptables-blacklist/master/blacklist.txt);
 do
+    echo - $ip
     iptables -A INPUT -s $ip -j DROP
     iptables -A OUTPUT -s $ip -j DROP
 done
