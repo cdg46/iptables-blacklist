d23cb73641ca69091c927a4312acc1b4632a16ef
Alexander Kress <alexanderkress@users.noreply.github.com>
2017-01-15 18:53:13 +0100

    Added counter to show amount of blocked addresses

--- iptables-blacklist.sh
+++ iptables-blacklist.sh
@@ -6,11 +6,14 @@ echo ===================
 echo
 echo The following IP addresses will be blocked:
 
+COUNTER=0
 for ip in $(wget -O- -q https://raw.githubusercontent.com/alexanderkress/iptables-blacklist/master/blacklist.txt);
 do
     echo - $ip
     iptables -A INPUT -s $ip -j DROP
     iptables -A OUTPUT -s $ip -j DROP
+    let COUNTER++
 done
+echo "$COUNTER IP addresses have been blocked"
 
 exit 0
